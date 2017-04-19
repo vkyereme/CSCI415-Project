cf468c2d8c78fa9a2be84693c191b926a503d0f4
Victoria Osei Kyereme <vkyereme@titan.cs.ndsu.nodak.edu>
2017-04-19 12:47:24 -0500

    Edited main function and included timer functions

--- passwordCrack.cu
+++ passwordCrack.cu
@@ -1,17 +1,21 @@
 #include<iostream>
+#include<string>
 #include<cstring>
 #include<ctime>
 #include<cstdlib>
+#include<sys/time.h>
+#include<stdio.h>
+#include<iomanip>
+
 using namespace std;
 
 int a[1000]; //array of all possible password characters
 int b[1000]; //array of attempted password cracks
 unsigned long long tries = 0;
-int length; //length of password
 char alphabet[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 
-void serial_passwordCrack(){
+void serial_passwordCrack(int length){
 bool cracked = false;
 do{
     b[0]++;
@@ -37,13 +41,34 @@ do{
 }
 
 
-__global__ void parallel_passwordCrack()
+__global__ void parallel_passwordCrack(int length)
 {
 
 }
 
+
+long long start_timer() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+
+// Prints the time elapsed since the specified time
+long long stop_timer(long long start_time, std::string name) {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	long long end_time = tv.tv_sec * 1000000 + tv.tv_usec;
+        std::cout << std::setprecision(5);	
+	std::cout << name << ": " << ((float) (end_time - start_time)) / (1000 * 1000) << " sec\n";
+	return end_time - start_time;
+}
+
+
+
 int main()
 {
+int length; //length of password
 int random; //random password to be generated
 
 cout << "Enter a password length: ";
@@ -51,18 +76,20 @@ cin >> length;
 
 
 srand(time(NULL));
+cout << "Random generated password: " << endl;
 for (int i =0; i<length; i++){
     
         random = alphabet[(rand()%26)]; 
     
     a[i] = random; //adding random password to array
     cout << char(a[i]);
-}cout << endl;
-serial_passwordCrack();
+}cout << "\n" << endl;
+cout << "Serial Password Cracked: " << endl;
+serial_passwordCrack(length);
 cout << "\n";
-for(int i=0; i<length; i++)
-    cout << char (b[i]);
-cout << "\nNumber of tries: " << tries << endl;
+for(int i=0; i<length; i++){
+    cout << char(b[i]);
+}cout << "\nNumber of tries: " << tries << endl;
 
 return 0;
 }
